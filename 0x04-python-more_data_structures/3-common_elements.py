09bd647c71aec31fe1faa7c7924b50b12321dbd4
keme-ebi <kelvin6bolou@gmail.com>
2022-12-23 23:45:42 +0000

    returns a set of common elements in two sets

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    first = set(set_1)
-    second = set(set_2)
-
-    if len(first.intersection(second)) > 1:
-        return (first.intersection(second))
+    return set_1 & set_2
