f16147a0578f29a460d54fb301ca33df030cab41
keme-ebi <kelvin6bolou@gmail.com>
2022-12-25 03:51:10 +0000

    replaces or adds key/value in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k, v in a_dictionary.items():
-        if k in a_dictionary and v not in a_dictionary:
-            a_dictionary.update({key: value})
-        elif k not in a_dictionary and v not in a_dictionary:
-            a_dictionary.update({key: value})
-        return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
