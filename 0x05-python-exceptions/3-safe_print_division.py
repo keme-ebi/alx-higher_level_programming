bdbf27436656ce000e7d6a566b9d19bc43fa64e6
keme-ebi <kelvin6bolou@gmail.com>
2022-12-31 09:48:30 +0000

    divides 2 integers and prints the result

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,7 +5,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        if a == 0 or b == 0:
+        if b == 0:
             print("Inside result: {}".format(None))
         else:
             print("Inside result: {}".format(a / b))
