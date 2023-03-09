ad3f9b1902c872520332343234038c9c77158c0b
keme-ebi <kelvin6bolou@gmail.com>
2023-03-09 18:49:19 +0000

    program that imports a function from a file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
-    sum = add_0.add(a, b)
+    sum = add(a, b)
 
     print("{} + {} = {}".format(a, b, sum))
