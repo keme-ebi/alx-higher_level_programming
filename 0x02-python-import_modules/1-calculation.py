22413a22fd3793995ad4c85fd067eefd53151e17
keme-ebi <kelvin6bolou@gmail.com>
2023-03-09 19:07:01 +0000

    a program that imports functions from a file and does some Maths

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    addi = add(a, b)
+    subt = sub(a, b)
+    mult = mul(a, b)
+    divi = div(a, b)
+
+    print("{} + {} = {}".format(a, b, addi))
+    print("{} - {} = {}".format(a, b, subt))
+    print("{} * {} = {}".format(a, b, mult))
+    print("{} / {} = {}".format(a, b, divi))
