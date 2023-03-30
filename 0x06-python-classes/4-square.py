57e6c9ea6aa55f0d149989409c7b6d770854d144
keme-ebi <kelvin6bolou@gmail.com>
2023-03-30 13:38:38 -0700

    a class Square that defines a square by private instance attribute, use property to retrieve the square size, property setter to set the size of square(checks if value is an integrr, if it is less than 0) and returns the area of the square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Defines a class Square by private instance attribute"""
+
+
+class Square:
+    """a class with private instance attribute"""
+    
+    def __init__(self, size=0):
+        """instantiation with optional size
+        Args:
+            size(int)=0: size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter
+        Args:
+            value(int): number to be put into size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """public instance method that returns tbe area of the square"""
+        return (self.__size * self.__size)
