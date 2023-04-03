8edf4c2673f3d04b6e01f45b4f82c7804e8b559a
keme-ebi <kelvin6bolou@gmail.com>
2023-04-03 08:13:50 -0700

    fix according to pycodestyle

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -70,8 +70,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
 
-        return '\n'.join(str(self.print_symbol) * self.__width
-                for i in range(self.height))
+        return '\n'.join
+    (str(self.print_symbol) * self.__width for i in range(self.height))
 
     def __repr__(self):
         """returns a string representation of the rectangle"""
