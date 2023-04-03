bc5a9da029afac1e02f8358fc1b715fc7be3cb49
keme-ebi <kelvin6bolou@gmail.com>
2023-04-03 06:22:27 -0700

    corrections according to pycodestyle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -11,8 +11,8 @@ class Rectangle:
             width(int): width of the rectangle
             height(int): height of the rectangle
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -39,8 +39,11 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """property setter
-        Args:                                                     value(int): value for rectangle height            """
-        if not isinstance(value, int):                            raise TypeError("height must be an integer")
+        Args:
+            value(int): value for rectangle height
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
