7bd61c7909874840f96898c780a98fa5b4b797c8
keme-ebi <kelvin6bolou@gmail.com>
2023-04-11 07:16:18 +0000

    add more test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,7 +8,33 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_normalinput(self):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_addition_in_list(self):
         self.assertEqual(max_integer([1, 3, 4 + 2, 2], 6)
+
+    def test_max_in_middle(self):
+        self.assertEqual(max_integer([1, 2, 7, 5, 3], 7)
+
+    def test_all_negative(self):
+        self.assertEqual(max_integer([-1, -4, -10, -20], -1)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([30, 10, 2], 30)
+
+    def test_max_at_end(self):
+        self.assertEqual(max_integer([12, 14, 20], 20)
+
+    def test_one_negative(self):
+        self.assertEqual(max_integer([1, 3, -10, 2], 3)
+
+    def test_one_element(self):
+        self.assertEqual(max_integer([1], 1)
+
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([])
+
+    def test_zero_args(self):
+        self.assertIsNone(max_integer())
 
 
 if __name__ == '__main__':
