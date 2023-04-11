dba79b7339b77a7efe68d9a76a0a00070e46f4cc
keme-ebi <kelvin6bolou@gmail.com>
2023-04-11 07:24:28 +0000

    fix syntax errors

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,28 +10,28 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_addition_in_list(self):
-        self.assertEqual(max_integer([1, 3, 4 + 2, 2], 6)
+        self.assertEqual(max_integer([1, 3, 4 + 2, 2]), 6)
 
     def test_max_in_middle(self):
-        self.assertEqual(max_integer([1, 2, 7, 5, 3], 7)
+        self.assertEqual(max_integer([1, 2, 7, 5, 3]), 7)
 
     def test_all_negative(self):
-        self.assertEqual(max_integer([-1, -4, -10, -20], -1)
+        self.assertEqual(max_integer([-1, -4, -10, -20]), -1)
 
     def test_max_at_beginning(self):
-        self.assertEqual(max_integer([30, 10, 2], 30)
+        self.assertEqual(max_integer([30, 10, 2]), 30)
 
     def test_max_at_end(self):
-        self.assertEqual(max_integer([12, 14, 20], 20)
+        self.assertEqual(max_integer([12, 14, 20]), 20)
 
     def test_one_negative(self):
-        self.assertEqual(max_integer([1, 3, -10, 2], 3)
+        self.assertEqual(max_integer([1, 3, -10, 2]), 3)
 
     def test_one_element(self):
-        self.assertEqual(max_integer([1], 1)
+        self.assertEqual(max_integer([1]), 1)
 
     def test_empty_list(self):
-        self.assertIsNone(max_integer([])
+        self.assertIsNone(max_integer([]))
 
     def test_zero_args(self):
         self.assertIsNone(max_integer())
