d8d6bda30994b8437eca916ccb94e6ba48555663
keme-ebi <kelvin6bolou@gmail.com>
2023-04-12 22:19:49 +0000

    a class Square that inherits from rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,10 @@ class Square(Rectangle):
     """inherits from ``9-rectangle``"""
     def __init__(self, size):
         """instantiation with size and invoking the subclass"""
-        Rectangle.__init__(self, size, size)
         self.integer_validator("size", size)
         self.__size = size
+
+        Rectangle.__init__(self, size, size)
 
     # override area from grandparent class
     def area(self):
