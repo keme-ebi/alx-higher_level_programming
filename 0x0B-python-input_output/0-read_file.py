e39ba4d9f1e561a37731c9063c03686b14c82398
keme-ebi <kelvin6bolou@gmail.com>
2023-04-13 00:56:51 +0000

    a function that reads a text file and prints it to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,8 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout
        Arg:
-           filename(file): file to be read
+            filename(file): file to be read
     """
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        for line in f:
+            print(line, end="")
