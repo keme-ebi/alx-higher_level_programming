b97d61fc39403c1e1a43da565567695adefe8f9a
keme-ebi <kelvin6bolou@gmail.com>
2023-04-13 00:51:59 +0000

    a function that reads a text file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module with one function that reads a text file"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+       Arg:
+           filename(file): file to be read
+    """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())
