f40fdb02a1834d6ee3b6abb79770fa90d5cfd99d
keme-ebi <kelvin6bolou@gmail.com>
2023-04-17 23:40:39 +0000

    added more tests for the modules

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Unittest for Rectangle class
 """
+from io import StringIO
+from contextlib import redirect_stdout
 import unittest
 from models.rectangle import Rectangle
 from models.base import Base
@@ -15,19 +17,13 @@ class TestRectangleInstances(unittest.TestCase):
             r1 = Rectangle(1)
 
     def test_2_args(self):
-        r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1.width, 10)
-        self.assertEqual(r1.height, 2)
-        self.assertEqual(r1.x, 0)
-        self.assertEqual(r1.y, 0)
-
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r2.width, 2)
-        self.assertEqual(r2.height, 10)
-        self.assertEqual(r2.x, 0)
-        self.assertEqual(r2.y, 0)
+        rt = Rectangle(10, 2)
+        rb = Rectangle(4, 3)
+        self.assertEqual(rt.id, rb.id - 1)
+        self.assertEqual(rt.width, 10)
+        self.assertEqual(rt.height, 2)
+        self.assertEqual(rt.x, 0)
+        self.assertEqual(rt.y, 0)
 
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
@@ -38,48 +34,199 @@ class TestRectangleInstances(unittest.TestCase):
 
     def test_passing_strings_to_instances_except_id(self):
         with self.assertRaises(TypeError):
-            r1 = Rectangle('1', 1)
+            Rectangle('1', 1)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, '1')
+            Rectangle(1, '1')
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, '1')
+            Rectangle(1, 1, '1')
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, 1, 'y')
+            Rectangle(1, 1, 1, 'y')
 
     def test_passing_floats_to_instances_except_id(self):
         with self.assertRaises(TypeError):
-            r1 = Rectangle(4.4, 1)
+            Rectangle(4.4, 1)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 2.2)
+            Rectangle(1, 2.2)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, 1.3)
+            Rectangle(1, 1, 1.3)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, 1, 1.5)
+            Rectangle(1, 1, 1, 1.5)
 
     def test_arg_more_than_expected(self):
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, 2, 3, 2, 2)
+            Rectangle(1, 1, 2, 3, 2, 2)
 
     def test_access_private_instances(self):
-        r1 = Rectangle(1, 1)
+        Rectangle(1, 1)
         with self.assertRaises(AttributeError):
-            r1.__width
+            Rectangle.__width
         with self.assertRaises(AttributeError):
-            r1.__width
+            Rectangle.__width
         with self.assertRaises(AttributeError):
-            r1.__x
+            Rectangle.__x
         with self.assertRaises(AttributeError):
-            r1.__y
+            Rectangle.__y
 
     def test_arg_with_tuples_list_sets(self):
         with self.assertRaises(TypeError):
-            r1 = Rectangle([1], 1)
+            Rectangle([1], 1)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, {1})
-        r1 = Rectangle(1, 1, (1))
-        self.assertEqual(r1.x, 1)
+            Rectangle(1, {1})
+        rl = Rectangle(1, 1, (1))
+        self.assertEqual(rl.x, 1)
         with self.assertRaises(TypeError):
-            r1 = Rectangle(1, 1, 1, {2})
+            Rectangle(1, 1, 1, {2})
+
+class TestRectangleArea(unittest.TestCase):
+
+    def test_area(self):
+        r5 = Rectangle(3, 2)
+        self.assertEqual(r5.area(), 6)
+        r5 = Rectangle(2, 10)
+        self.assertEqual(r5.area(), 20)
+        r5 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r5.area(), 56)
+
+class TestRectangleDisplay(unittest.TestCase):
+
+    def test_display(self):
+        expected_output = "##\n##\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rd = Rectangle(2, 2)
+            rd.display()
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_display_when_x_or_y_or_both_are_present(self):
+        expected_output = "\n\n  ##\n  ##\n  ##\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rd = Rectangle(2, 3, 2, 2)
+            rd.display()
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestRectangleSTRMethod(unittest.TestCase):
+
+    def test_str_mehthod(self):
+        expected_output = "[Rectangle] (12) 2/1 - 4/6\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs = Rectangle(4, 6, 2, 1, 12)
+            print(rs)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_str_method_with_no_id_added(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5, 1)
+            rse = Rectangle(4, 4)
+            expected_output = "[Rectangle] ({}) 1/0 - 5/5\n".format(rse.id - 1)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestRectangleUpdateMethod(unittest.TestCase):
+
+    def test_normal_method_instances_except_id(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs = Rectangle(10, 10, 10, 10)
+            rse = Rectangle(5, 5)
+            expected_output = "[Rectangle] ({}) 10/10 - 10/10\n".format\
+                (rse.id - 1)
+            print(rs)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_id_only(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5)
+            rs2.update(89)
+            expected_output = "[Rectangle] ({}) 0/0 - 5/5\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_id_height_and_width(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(10, 10)
+            rs2.update(89, 2, 3)
+            expected_output = "[Rectangle] ({}) 0/0 - 2/3\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_id_height_width_and_x(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(10, 10)
+            rs2.update(89, 2, 3, 4)
+            expected_output = "[Rectangle] ({}) 4/0 - 2/3\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_all_instances(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(10, 10)
+            rs2.update(89, 2, 3, 4, 5)
+            expected_output = "[Rectangle] ({}) 4/5 - 2/3\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_without_kwargs(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 1)
+            rs2.update(89)
+            expected_output = "[Rectangle] ({}) 0/0 - 5/1\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_height_kwarg(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5)
+            rs2.update(89)
+            rs2.update(height=1)
+            expected_output = "[Rectangle] ({}) 0/0 - 5/1\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_width_and_x(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5)
+            rs2.update(89)
+            rs2.update(width=1, x=2)
+            expected_output = "[Rectangle] ({}) 2/0 - 1/5\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_of_with_width_y_x_and_id(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5)
+            rs2.update(y=1, width=2, x=3, id=89)
+            expected_output = "[Rectangle] ({}) 3/1 - 2/5\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_all_instances_with_kwargs(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(5, 5)
+            rs2.update(89)
+            rs2.update(x=1, height=2, y=3, width=1)
+            expected_output = "[Rectangle] ({}) 1/3 - 1/2\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_adding_both_args_and_kwargs(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            rs2 = Rectangle(10, 10, 10, 10)
+            rs2.update(89, height=1)
+            expected_output = "[Rectangle] ({}) 10/10 - 10/10\n".format(rs2.id)
+            print(rs2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestRectangleToDictionary(unittest.TestCase):
+
+    def test_to_dictionary(self):
+        expected_output = {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}
+        rt = Rectangle(10, 2, 1, 9, 1)
+        rtd = rt.to_dictionary()
+        self.assertDictEqual(rtd, expected_output)
+
+    def test_to_dictionary_check_type(self):
+        expected_output = {'x': 1, 'y': 9, 'id': 1, 'height': 2, 'width': 10}
+        rt = Rectangle(10, 2, 1, 9, 1)
+        rtd = rt.to_dictionary()
+        self.assertEqual(type(rtd), dict)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,235 @@
+#!/usr/bin/python3
+"""Unittest for Rectangle class
+"""
+from io import StringIO
+from contextlib import redirect_stdout
+import unittest
+from models.rectangle import Rectangle
+from models.square import Square
+from models.base import Base
+
+class TestSquareInstances(unittest.TestCase):
+
+    def test_if_square_is_subclass_of_base(self):
+        self.assertEqual(issubclass(Square, Base), True)
+
+    def test_if_square_is_subclass_of_rectangle(self):
+        self.assertEqual(issubclass(Square, Rectangle), True)
+
+    def test_1_arg(self):
+         s1 = Square(1)
+         self.assertEqual(s1.size, 1)
+
+    def test_2_args(self):
+        st = Square(10, 2)
+        sb = Square(4, 3)
+        self.assertEqual(st.id, sb.id - 1)
+        self.assertEqual(st.size, 10)
+        self.assertEqual(st.x, 2)
+        self.assertEqual(st.y, 0)
+
+        s3 = Square(10, 2, 0, 12)
+        self.assertEqual(s3.id, 12)
+        self.assertEqual(s3.size, 10)
+        self.assertEqual(s3.x, 2)
+        self.assertEqual(s3.y, 0)
+
+    def test_passing_strings_to_instances_except_id(self):
+        with self.assertRaises(TypeError):
+            Square('1', 1)
+        with self.assertRaises(TypeError):
+            Square(1, '1')
+        with self.assertRaises(TypeError):
+            Square(1, 1, '1')
+
+    def test_passing_floats_to_instances_except_id(self):
+        with self.assertRaises(TypeError):
+            Square(4.4, 1)
+        with self.assertRaises(TypeError):
+            Square(1, 2.2)
+        with self.assertRaises(TypeError):
+            Square(1, 1, 1.3)
+        with self.assertRaises(TypeError):
+            Square(1, 1, 1.5)
+
+    def test_arg_more_than_expected(self):
+        with self.assertRaises(TypeError):
+            Square(1, 1, 2, 3, 2)
+
+    def test_access_private_instances(self):
+        Square(1)
+        with self.assertRaises(AttributeError):
+            Square.__size
+
+    def test_private_width(self):
+        Square(5)
+        with self.assertRaises(AttributeError):
+            Square.__width
+
+    def test_private_height(self):
+        Square(2)
+        with self.assertRaises(AttributeError):
+            Square.__height
+
+    def test_private_x(self):
+        Square(7)
+        with self.assertRaises(AttributeError):
+            Square.__x
+
+    def test_private_y(self):
+        Square(8)
+        with self.assertRaises(AttributeError):
+            Square.__y
+
+    def test_arg_with_tuples_list_sets(self):
+        with self.assertRaises(TypeError):
+            Square([1], 1)
+        with self.assertRaises(TypeError):
+            Square(1, {1})
+        sl = Square(1, 1, (1))
+        self.assertEqual(sl.y, 1)
+        with self.assertRaises(TypeError):
+            Square(1, 1, {2}, 1)
+
+class TestSquareArea(unittest.TestCase):
+
+    def test_area(self):
+        s5 = Square(3)
+        self.assertEqual(s5.area(), 9)
+        s5 = Square(2, 4, 5)
+        self.assertEqual(s5.area(), 4)
+        s5 = Square(8, 4)
+        self.assertEqual(s5.area(), 64)
+
+class TestSquareDisplay(unittest.TestCase):
+
+    def test_display(self):
+        expected_output = "##\n##\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            sd = Square(2)
+            sd.display()
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_display_with_x(self):
+        expected_output = "  ##\n  ##\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            sd = Square(2, 2)
+            sd.display()
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_display_when_x_or_y_or_both_are_present(self):
+        expected_output = "\n\n  ##\n  ##\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            sd = Square(2, 2, 2)
+            sd.display()
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestSquareSTRMethod(unittest.TestCase):
+
+    def test_str_mehthod(self):
+        expected_output = "[Square] (12) 6/2 - 4\n"
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss = Square(4, 6, 2, 12)
+            print(ss)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_str_method_with_no_id_added(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5, 5, 1)
+            sse = Square(4, 4)
+            expected_output = "[Square] ({}) 5/1 - 5\n".format(sse.id - 1)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestSquareUpdateMethod(unittest.TestCase):
+
+    def test_normal_method_instances_except_id(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss = Square(10, 10, 10)
+            sse = Square(5, 5)
+            expected_output = "[Square] ({}) 10/10 - 10\n".format(sse.id - 1)
+            print(ss)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_id_only(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5)
+            ss2.update(89)
+            expected_output = "[Square] ({}) 0/0 - 5\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_id_size_and_x(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(10)
+            ss2.update(89, 2, 3)
+            expected_output = "[Square] ({}) 3/0 - 2\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_all_instances(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(10)
+            ss2.update(89, 2, 3, 4)
+            expected_output = "[Square] ({}) 3/4 - 2\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_without_kwargs(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5)
+            ss2.update(89)
+            expected_output = "[Square] ({}) 0/0 - 5\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_size_kwarg(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5)
+            ss2.update(89)
+            ss2.update(size=1)
+            expected_output = "[Square] ({}) 0/0 - 1\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_with_size_and_x(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5, 5)
+            ss2.update(89)
+            ss2.update(size=1, x=2)
+            expected_output = "[Square] ({}) 2/0 - 1\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_update_method_kwargs_with_all_instances(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(5, 5)
+            ss2.update(y=1, size=2, x=3, id=89)
+            expected_output = "[Square] ({}) 3/1 - 2\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+    def test_adding_both_args_and_kwargs(self):
+        with StringIO() as buffer, redirect_stdout(buffer):
+            ss2 = Square(10, 10, 10)
+            ss2.update(89, size=1)
+            expected_output = "[Square] ({}) 10/10 - 10\n".format(ss2.id)
+            print(ss2)
+            self.assertEqual(buffer.getvalue(), expected_output)
+
+class TestSquareToDictionary(unittest.TestCase):
+
+    def test_to_dictionary(self):
+        expected_output = {'x': 2, 'y': 1, 'id': 1, 'size': 10}
+        st = Square(10, 2, 1, 1)
+        std = st.to_dictionary()
+        self.assertDictEqual(std, expected_output)
+
+    def test_to_dictionary_check_type(self):
+        expected_output = {'x': 2, 'y': 1, 'id': 9, 'size': 10}
+        st = Square(10, 2, 1, 9)
+        std = st.to_dictionary()
+        self.assertEqual(type(std), dict)
+
+if __name__ == "__main__":
+    unittest.main()
