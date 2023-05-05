bcf9e16b51accde532d0c30ea2a9b3e6e09cc031
keme-ebi <kelvin6bolou@gmail.com>
2023-05-05 23:17:37 +0000

    a function that reverses == and != operators

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""MyInt module"""
+
+
+class MyInt:
+    """MyInt class that reverses the == and != operators"""
+    def __init__(self, value):
+        """initialisation"""
+        self.value = value
+
+    def __str__(self):
+        """returns sting representation"""
+        return str(self.value)
+
+    def __eq__(self, other):
+        """changes == to != operator"""
+        return self.value != other
+
+    def __ne__(self, other):
+        """changes != to == operator"""
+        return self.value == other
