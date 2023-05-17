183b28aa7214b3b4efa56986357ef260bbf8598c
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 21:42:20 +0000

    a script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates the MySQL server user `user_0d_1`
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- sets user_0d_1 to have all privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
