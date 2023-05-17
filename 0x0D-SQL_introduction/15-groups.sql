5040d53626e810f6053eb48d01264728ded96143
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 00:55:49 +0000

    lists the number of records with the same score in the table second_table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- lists the number of records with the same score in the table `second_table`
 -- displays the scores in descending order
-SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS 'number' FROM second_table GROUP BY score ORDER BY number DESC;
