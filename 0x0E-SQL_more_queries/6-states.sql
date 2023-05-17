0f82077a54566e836c713c46ee6199018f4490d0
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 22:28:47 +0000

    creates a database hbtn_0d_usa with the table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- create the table 'states'
+-- create the table 'states' in the hbtn_0d_usa database
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
 	(id INT PRIMARY KEY, name VARCHAR(256) NOT NULL);
