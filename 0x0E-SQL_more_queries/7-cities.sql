a5ce4d5a99445cd7c4e47d1604ab47df5f7d443e
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 22:38:10 +0000

    creates the database hbtn_0d_usa and the table cities with a state_id column that references to the id of the states table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create the table 'cities' in the hbtn_0d_usa database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+	(id INT AUTO_INCREMENT PRIMARY KEY,
+		FOREIGN KEY(state_id) REFERENCES states(id),
+		name VARCHAR(256) NOT NULL);
