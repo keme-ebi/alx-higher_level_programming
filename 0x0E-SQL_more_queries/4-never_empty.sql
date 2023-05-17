f4b1f2878927f38e77ba898b7807467424582136
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 22:11:40 +0000

    creates the table `id_not_null` and sets id to a default value 1

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,2 @@
 -- creates the table 'id_not_null' with a default id value of 1
-CREATE TABLE IF NOT EXISTS id_not_null (id INT, name VARCHAR(256))
-	VALUES(1);
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
