66a82c295b89ffab99a0789a3843e864fadead81
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 00:12:35 +0000

    lists all records of the table second_table of the hbtn_0c_0 database, displaying the top score first

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- displays all records of the table second_table
+-- displays in the order (score and name)
+-- records are ordered by the top score
+SELECT score, name FROM second_table ORDER BY score DESC, name DESC;
