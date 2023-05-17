722367915ecb6f234b69399ff04ebce35ee2a3de
keme-ebi <kelvin6bolou@gmail.com>
2023-05-17 23:19:19 +0000

    a script that lists all cities contained in the database hbtn_0d_usa, displayed as cities.id, cities.name, states.name

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- lists all cities of California that can be found in the database hbtn_0d_usa
+-- lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name FROM cities
 	INNER JOIN states USING (states.id)
 	ORDER BY cities.id ASC;
