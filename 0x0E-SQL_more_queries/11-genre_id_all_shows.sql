bd08d6be0ba1a1c9ca8a217be29a06fe2817a831
keme-ebi <kelvin6bolou@gmail.com>
2023-05-19 22:11:34 +0000

    display all shows cointained in the database

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- lists all shows contained in hbtn_0d_tvshowa that have at least 1 genre linked
+-- display: tv_shows.title - tv_show_genres.genre_id
+-- results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT title, genre_id
+	FROM tv_show_genres NATURAL JOIN tv_shows
+	ORDER BY title ASC, genre_id ASC;
