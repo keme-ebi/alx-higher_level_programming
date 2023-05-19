bf507062100358c5c069d63901e01d0e2422f033
keme-ebi <kelvin6bolou@gmail.com>
2023-05-19 23:10:11 +0000

    display all shows cointained in the database

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshowa that have at least 1 genre linked
 -- display: tv_shows.title - tv_show_genres.genre_id
+-- if a show doesn't have a genre, it returns NULL
 -- results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT title, genre_id
-	FROM tv_shows
-	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
-	ORDER BY title, genre_id ASC;
+	FROM tv_show_genres
+	RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	ORDER BY title ASC, genre_id ASC;
