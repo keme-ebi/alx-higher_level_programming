32a7247e885ef72e974551d191dc092653f1a818
keme-ebi <kelvin6bolou@gmail.com>
2023-05-19 22:21:17 +0000

    display all shows cointained in the database

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,6 @@
 -- display: tv_shows.title - tv_show_genres.genre_id
 -- results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT title, genre_id
-	FROM tv_show_genres NATURAL JOIN tv_shows
+	FROM tv_show_genres
+	RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id
 	ORDER BY title ASC, genre_id ASC;
