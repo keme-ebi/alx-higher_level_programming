b0545ae5c53a589644446ee94c4c6cf292ed1881
keme-ebi <kelvin6bolou@gmail.com>
2023-05-19 21:53:46 +0000

    lists all shows contained in hbtn_0d_tvshows that have at least one genre linked

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshowa that have at least 1 genre linked
 -- display: tv_shows.title - tv_show_genres.genre_id
 -- results sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT tv_shows.title AS 'title', tv_show_genres.genre_id AS 'genre_id'
-	FROM tv_show_genres
-	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-	ORDER BY tv_shows.tile ASC, tv_show_genres.genre_id ASC;
+SELECT title, genre_id
+	FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	ORDER BY tile ASC, genre_id ASC;
