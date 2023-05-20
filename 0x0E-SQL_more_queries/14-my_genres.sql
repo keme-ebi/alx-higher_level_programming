8f61f484d349e1c24fd2bd19266d58f1dd9d7da6
keme-ebi <kelvin6bolou@gmail.com>
2023-05-20 00:34:10 +0000

    lists all genres of the show Dexter

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,8 +4,6 @@
 SELECT name
 	FROM tv_genres
 	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id
 	WHERE title = 'Dexter'
-		(FROM tv_shows
-			INNER JOIN tv_show_genres
-			ON tv_shows.id = tv_show_genres.genre_id)
-	ORDER BY name DESC;
+	ORDER BY name ASC;
