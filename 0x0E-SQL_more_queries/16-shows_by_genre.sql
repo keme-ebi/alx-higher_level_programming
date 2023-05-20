45384f0ef348e6deef0ffba22e6dbb200514d588
keme-ebi <kelvin6bolou@gmail.com>
2023-05-20 00:54:40 +0000

    lists all shows, and all genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,8 @@
+-- lists all Comedy shows, and all genres linked to that show from hbtn_0d_tvshowa database
+-- display: tv_shows.title - tv_genres.name
+-- results sorted in ascending order by the show title and genre name
+SELECT title, name
+	FROM tv_shows
+	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	ORDER BY title ASC, name ASC;
