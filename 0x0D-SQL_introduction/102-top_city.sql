ae72bccc85bd4fdef8435c14b25c5687d47e1499
keme-ebi <kelvin6bolou@gmail.com>
2023-05-20 02:22:07 +0000

    changed position of limit clause

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,5 +2,6 @@
 SELECT city, AVG(value) AS 'avg_temp'
 	FROM temperatures
 	WHERE month = 'July' OR month = 'August'
-	GROUP BY city LIMIT 3
-	ORDER BY avg_temp DESC;
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
