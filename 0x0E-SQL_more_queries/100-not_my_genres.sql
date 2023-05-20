3a6c5f76f5ef1f67d8c6fecd0d2db1458f82bdcf
keme-ebi <kelvin6bolou@gmail.com>
2023-05-20 02:45:01 +0000

    a script that displays all genres not linked to the show Dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,7 +3,7 @@
 -- results sorted in ascending order by the genre name
 SELECT name
 	FROM tv_genres
-	RIGHT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-	RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	LEFT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 	WHERE title = 'Dexter'
 	ORDER BY name ASC;
