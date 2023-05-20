015f99dc75b31510437b8e6debfd9f3f590a8af5
keme-ebi <kelvin6bolou@gmail.com>
2023-05-20 01:00:24 +0000

    lists all shows, and all genres linked to that show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,5 @@
 SELECT title, name
 	FROM tv_shows
 	LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 	ORDER BY title ASC, name ASC;
