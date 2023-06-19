60b35677aacc2715d0d79802a74822b1d00860a8
keme-ebi <kelvin6bolou@gmail.com>
2023-06-19 16:37:55 +0000

    a script that adds the State object `Louisiana` to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""lists all state objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = {
+        'name': 'Louisiana'
+    }
+
+    new = State(**new_state)
+
+    session.add(new)
+
+    session.commit()
+
+    state = session.query(State).order_by(State.id.asc()).all()
+
+    for st in state:
+        print("{}: {}".format(st.id, st.name))
+
+    session.close()
