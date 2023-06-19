52045d2ed8205c6ff6423f4658f33f524360e214
keme-ebi <kelvin6bolou@gmail.com>
2023-06-19 23:56:00 +0000

    improve the files model_city.py and model_state.py, and save them as relationship_city.py and relationship_state.py, added a script that creates the State California with the city San Francisco from the database hbtn_0e_100_usa

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""adds state object Louisiana to the database hbtn_0e_6_usa"""
+
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    newst = State(name='California')
+
+    cty = City(name='San Francisco')
+
+    newst.cities.append(cty)
+
+    session.add(newst)
+    session.add(cty)
+
+    session.commit()
+
+    session.close()
