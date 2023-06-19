924740341e9b5b40b52d60d6a8b1981143776098
keme-ebi <kelvin6bolou@gmail.com>
2023-06-19 16:58:43 +0000

    changed documentation

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all state objects from the database hbtn_0e_6_usa"""
+"""adds state object Louisiana to the database hbtn_0e_6_usa"""
 
 from model_state import Base, State
 from sqlalchemy import (create_engine)
