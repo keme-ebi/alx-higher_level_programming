f29d561b58422359ff2a44b124a0c3b7f945703f
Keme-ebi Remember Bolou <105162424+keme-ebi@users.noreply.github.com>
2023-06-20 01:33:55 +0100

    Update 1-filter_states.py by adding % before N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module lists all states with a name starting with \
-        N from the database hbtn_0e_0_usa"""
+"""This module lists all states with a name starting with
+N from the database hbtn_0e_0_usa"""
 
 
 import MySQLdb
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.\
+    cur.execute("SELECT * FROM states WHERE name LIKE '%N%' ORDER BY states.\
             id ASC")
 
     for row in cur.fetchall():
