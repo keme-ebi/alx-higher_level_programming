fb36181ab783f0cb2c336c3f1d63a61bc9403430
Keme-ebi Remember Bolou <105162424+keme-ebi@users.noreply.github.com>
2023-06-20 01:31:10 +0100

    Update database name in documentation

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all state objects, and corresponding city objects in hbtn_0e_6_usa"""
+"""lists all state objects, and corresponding city objects in hbtn_0e_101_usa"""
 
 from relationship_state import Base, State
 from relationship_city import City
