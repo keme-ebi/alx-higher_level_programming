3b9e7f7bb231b041ba96b7a50fc148d4a7d5b4a5
keme-ebi <kelvin6bolou@gmail.com>
2023-07-03 04:01:46 +0000

    inserted utf-8 to decode()

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print('Body response:')
-    print('    - type: ', type(body))
-    print('    - content: ', body)
-    print('    - utf8 content: ', body.decode())
+    print('\t- type: ', type(body))
+    print('\t- content: ', body)
+    print('\t- utf8 content: {}', body.decode('utf-8'))
