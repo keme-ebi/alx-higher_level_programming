666187eb80f0d99550ca257e954b1c3084faa000
keme-ebi <kelvin6bolou@gmail.com>
2023-07-03 03:50:12 +0000

    a python script that sends a request to a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a module that fetches https://alx-intranet.hbtn.io/status using urllib"""
+
+import urllib.request
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+    print('Body response:')
+    print('\t- type: ', type(body))
+    print('\t- content: ', body)
+    print('\t- utf-8 content: ', body.decode())
