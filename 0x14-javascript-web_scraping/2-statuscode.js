eccc5ca0192fc01ea67d1db46de1153db68bffff
keme-ebi <kelvin6bolou@gmail.com>
2024-01-04 18:06:11 +0000

    handled expected error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
   console.log('code:', response.statusCode);
 });
